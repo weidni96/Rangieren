a27b7e6117ef53ef3e1b1485712ff48bbbff083a
weidni96 <viechers@web.de>
2016-04-20 23:13:31 +0200

    Added files via upload

--- Main.java
+++ Main.java
@@ -3,12 +3,15 @@ package Aufgabe_16;
 public class Main {
 	public static void main(String[]args){
 		Eingabe e = new Eingabe();
-		boolean weiterSpielen = e.weiterSpielen();
+		boolean randomGame = e.randomGame();
 		int[] numbers;
 		Gleis g;
 		
-		if(!weiterSpielen)numbers = e.numberInput();	
+		if(!randomGame)numbers = e.numberInput();	
 		else numbers = e.numberRandom();
+		for(int i = 0; i < numbers.length; i++){
+			System.out.println((i+1) + ": " + numbers[i]);
+		}
 		
 		g = new Gleis(numbers, weiterSpielen);
 		g.starten();
